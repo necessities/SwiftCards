9d50fcd42335a808b6166837218d21c03edb0bf6
necessities <bear_necessities@icloud.com>
2021-02-24 08:22:44 -0500

    Changed UI colors to white and black + minor wording changes in questions

--- SwiftCards/Model/QuizBrain.swift
+++ SwiftCards/Model/QuizBrain.swift
@@ -17,16 +17,9 @@ struct QuizBrain {
     
     let quiz = [
         
-        Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], ca: "Skin"),
-        Question(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], ca: "100"),
-        Question(q: "What do the letters in the GMT time zone stand for?", a: ["Global Meridian Time", "Greenwich Mean Time", "General Median Time"], ca: "Greenwich Mean Time"),
-        Question(q: "What is the French word for 'hat'?", a: ["Chapeau", "Écharpe", "Bonnet"], ca: "Chapeau"),
-        Question(q: "In past times, what would a gentleman keep in his fob pocket?", a: ["Notebook", "Handkerchief", "Watch"], ca: "Watch"),
-        Question(q: "How would one say goodbye in Spanish?", a: ["Au Revoir", "Adiós", "Salir"], ca: "Adiós"),
-        Question(q: "Which of these colours is NOT featured in the logo for Google?", a: ["Green", "Orange", "Blue"], ca: "Orange"),
-        Question(q: "What alcoholic drink is made from molasses?", a: ["Rum", "Whisky", "Gin"], ca: "Rum"),
-        Question(q: "What type of animal was Harambe?", a: ["Panda", "Gorilla", "Crocodile"], ca: "Gorilla"),
-        Question(q: "Where is Tasmania located?", a: ["Indonesia", "Australia", "Scotland"], ca: "Australia")
+        Question(q: "How do you declare a Variable?", a: ["Variable numberOfChickens = 5", "var numberOfCanonBalls = 10", "let numberOfShoes == 5"], ca: "var numberOfCanonBalls = 10"),
+        Question(q: "Which of the following is a declaration of a constant?", a: ["var five = 5", "const = 5", "let five = 5"], ca: "let five = 5"),
+        
         
         ]
     
