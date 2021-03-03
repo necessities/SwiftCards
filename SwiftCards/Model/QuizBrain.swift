d9e3e4ec93d15ddb91cc67d9fcfefb60871bf09e
“necessities” <bear_necessities@icloud.com>
2021-03-03 12:45:54 -0500

    Added more questions

--- SwiftCards/Model/QuizBrain.swift
+++ SwiftCards/Model/QuizBrain.swift
@@ -19,6 +19,11 @@ struct QuizBrain {
         
         Question(q: "How do you declare a Variable?", a: ["Variable numberOfChickens = 5", "var numberOfCanonBalls = 10", "let numberOfShoes == 5"], ca: "var numberOfCanonBalls = 10"),
         Question(q: "Which of the following is a declaration of a constant?", a: ["var five = 5", "const = 5", "let five = 5"], ca: "let five = 5"),
+        Question(q: "What is the command to print a string to the console?",a: ["printf(“Hello World”);", "print(“Hello World”)", "print(Hello World)"], ca: "print(“Hello World”)"),
+        Question(q: "A constant or variable must have the same type as the value you want to assign it.", a: ["True", "Only if it is marked explicitly", "False"], ca: "True"),
+        Question(q: "In Swift; constant to variable is the same as immutable to mutable", a: ["False", "Only if it is marked explicitly", "True"], ca: "True"),
+        Question(q: "Identify: shoppingList = []", a: ["This is a empty array", "This is a empty dictionary", "This is a variable set to nil"], ca: "This is a empty array"),
+        Question(q: "Identify: occupations = [:]", a: ["This is a empty array", "This is a empty dictionary", "This is a constant set to nil"], ca: "This is a empty dictionary"),
         
         
         ]
